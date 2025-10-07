154095b2dbaf30b53fe9c22f5cd83c34581ab587
harshitsinghai <harshitsinghai77@gmail.com>
2025-10-07 11:36:30 +0530

    initialize ecs batch

---
+++ infrastructure/ecs_fargate_stack.py
@@ -0,0 +1,64 @@
+from aws_cdk import (
+    Stack,
+    aws_ecs as _ecs,
+    aws_iam as _iam,
+    aws_logs as _logs,
+    aws_ec2 as _ec2,
+    RemovalPolicy,
+    CfnOutput,
+)
+from constructs import Construct
+
+class NemoAIECSFargateStack(Stack):
+    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+
+        vpc = _ec2.Vpc.from_lookup(self, "VPC", is_default=True)
+
+        cluster = _ecs.Cluster(self,"NemoAIECSCluster", vpc=vpc)
+
+        log_group = _logs.LogGroup(
+            self, "NemoAIContainerLogGroup",
+            log_group_name="/ecs/nemo-ai-container",
+            retention=_logs.RetentionDays.ONE_WEEK,
+            removal_policy=RemovalPolicy.DESTROY
+        )
+        log_driver = _ecs.LogDriver.aws_logs(
+            stream_prefix="nemo-ai-ecs",
+            log_group=log_group
+        )
+
+        task_role = _iam.Role(self, "NemoAIECSTaskRole",
+            assumed_by=_iam.ServicePrincipal("ecs-tasks.amazonaws.com"),
+            managed_policies=[
+            _iam.ManagedPolicy.from_aws_managed_policy_name("service-role/AmazonECSTaskExecutionRolePolicy")
+        ]
+        )
+        task_role.add_to_policy(_iam.PolicyStatement(
+            actions=[
+                "bedrock:InvokeModel",
+                "bedrock:InvokeModelWithResponseStream"
+            ],
+            resources=["*"],
+        ))
+
+        task_definition = _ecs.FargateTaskDefinition(self, "NemoAIECSTaskDefinition",
+            memory_limit_mib=1024,
+            cpu=512,
+            execution_role=task_role,
+            task_role=task_role
+        )
+        task_definition.add_container(
+            "NemoAIECSContainer",
+            image=_ecs.ContainerImage.from_registry(
+                "850995537443.dkr.ecr.us-east-1.amazonaws.com/cdk-hnb659fds-container-assets-850995537443-us-east-1:2294d4c0c5b81a5d373d1dc65096028e2cf50184cbe0df19fa05c7911ebec162"
+            ),
+            logging=log_driver,
+            memory_limit_mib=1024,
+            cpu=512,
+        )
+
+        CfnOutput(self, "TaskDefinitionArn", value=task_definition.task_definition_arn,  description="Task Definition ARN for Nemo AI ECS Fargate Task")
+        CfnOutput(self, "TaskRoleArn", value=task_role.role_arn, description="Task Role ARN for Nemo AI ECS Fargate Task")
+        CfnOutput(self, "ClusterName", value=cluster.cluster_name, description="Cluster Name for Nemo AI ECS Fargate Task")
+
